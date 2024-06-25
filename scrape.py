257a221e471b7d6646229a11e8ac18b0d3d25e8a
t-mio <takasshhi21@gmail.com>
2024-06-25 18:05:21 +0200

    botos3

--- scrape.py
+++ scrape.py
@@ -1,48 +1,42 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 import csv
+import boto3
 from datetime import datetime, timedelta
 
-# get date as format'YY-MM-DD'
-today = datetime.now().strftime('%Y-%m-%d')
-yesterday = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
+def lambda_handler(event, context):
+    # get date as format 'YY-MM-DD'
+    yesterday = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
 
-# function to fetch data in URL
-def fetch_data(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # check whether the response was returned
-        return response
-    except requests.exceptions.HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')  # HTTP error
-        return None
-    except Exception as err:
-        print(f'Other error occurred: {err}')  # Other error
-        return None
+    # function to fetch data in URL
+    def fetch_data(url):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # check whether the response was returned
+            return response
+        except requests.exceptions.HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')  # HTTP error
+            return None
+        except Exception as err:
+            print(f'Other error occurred: {err}')  # Other error
+            return None
 
-# URL of today
-url_today = f'https://www.oricon.co.jp/rank/js/d/{today}/'
+    # URL of yesterday
+    url = f'https://www.oricon.co.jp/rank/js/d/{yesterday}/'
 
-# fetch data from today URL
-response = fetch_data(url_today)
+    # fetch data from URL
+    response = fetch_data(url)
 
-# except today URL is None, fetch data from yesterday URL
-if response is None:
-    url_yesterday = f'https://www.oricon.co.jp/rank/js/d/{yesterday}/'
-    response = fetch_data(url_yesterday)
-
-# if URL is exist, parse HTML with BS4
-if response is not None:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    # extract class="box-rank-entry"
-    rank_entries = soup.find_all(class_='box-rank-entry')
-    
-    # Open a CSV file to write
-    with open('ranking.csv', mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        # Write the header
-        writer.writerow(['Status', 'Title', 'Name'])
+    # if URL exists, parse HTML with BS4
+    if response is not None:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # extract class="box-rank-entry"
+        rank_entries = soup.find_all(class_='box-rank-entry')
+        
+        # Create CSV content
+        csv_content = "Status,Title,Name\n"
         
         for entry in rank_entries:
             # get ranking fluctuations
@@ -57,8 +51,23 @@ if response is not None:
             name_tag = entry.find('p', class_='name')
             name = name_tag.text.strip() if name_tag else 'N/A'
             
-            # Write the row to CSV
-            writer.writerow([status, title, name])
-            
-else:
-    print("Could not retrieve data.")
+            # Add row to CSV content
+            csv_content += f"{status},{title},{name}\n"
+        
+        # upload to S3
+        s3 = boto3.client('s3')
+        bucket_name = "scrape-portfolio"
+        now = datetime.now().strftime("%Y%m%d") 
+        file_name = f"{now}.csv"
+        s3.put_object(Bucket=bucket_name, Key=file_name, Body=csv_content)
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps('CSV file created and uploaded to S3')
+        }
+                
+    else:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Could not retrieve data.')
+        }
